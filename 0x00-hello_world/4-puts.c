33f86b87f167efd6a34dfd67e4973101d219e176
Omar Khaddaoui <omar-khadd@hotmail.fr>
2023-09-07 19:38:15 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - program that print a text with a puts functions
- * Return: 0 (Success)
- * /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+* main - program that print a text with a puts functions
+* Return: 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
