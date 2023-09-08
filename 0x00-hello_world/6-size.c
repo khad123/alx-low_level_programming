912bca85f289b4de1ed012dda288b348d4cf6f51
Omar Khaddaoui <omar-khadd@hotmail.fr>
2023-09-07 20:03:56 -0700

    update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int i;
 long int ic;
 long long int d;
 float f;
-printf("Size of a Char: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
 printf("Size of a int: %lu byte(s)\n", (unsigned long) sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(ic));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
