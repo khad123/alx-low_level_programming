621d39a04fb9c5fe39bd041843f67a7b09012884
omar khaddaoui <omar-khadd@hotmail.fr>
2023-10-09 18:57:06 -0700

    update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: 0 (Success)
- *
- *int main(void)
- {
-        char alpha;
-
-        alpha = 'a';
-        while(alpha <= 'z')
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	
+	alpha = 'a';
+	while(alpha <= 'z')
         {
-                putchar(alpha);
-                alpha++;
+		putchar(alpha);
+		alpha++;
         }
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
  }
