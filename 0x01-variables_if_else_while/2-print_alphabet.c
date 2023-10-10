7960919e6ba701451312d4149189b87ee32ed87a
omar khaddaoui <omar-khadd@hotmail.fr>
2023-10-09 18:48:43 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ *
+ *int main(void)
+ {
+        char alpha;
+
+        alpha = 'a';
+        while(alpha <= 'z')
+        {
+                putchar(alpha);
+                alpha++;
+        }
+        putchar('\n');
+        return (0);
+ }
