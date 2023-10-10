5585fb0dccec5ad0e826c7517911ded85e8f348e
omar khaddaoui <omar-khadd@hotmail.fr>
2023-10-09 19:41:53 -0700

    add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a, b;
+
+	a = 'a';
+	b = 'A';
+	while
+		(a <= 'z') {
+			putchar(a);
+			a++;
+		}
+	while
+		(b <= 'Z') {
+			putchar(b);
+			b++
+		}
+	putchar('\n');
+	return (0);
+}
