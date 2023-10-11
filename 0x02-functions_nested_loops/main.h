f1105e0c044476d7e8094fba6cf1f97e32b6ffd4
omar khaddaoui <omar-khadd@hotmail.fr>
2023-10-10 19:24:01 -0700

    update 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
-int _putchat(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +13,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
 #endif
